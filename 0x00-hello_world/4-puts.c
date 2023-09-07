b389ff5ff3d8644e08b59cae8ae15002b9443ece
annmulwa <annmulwa2000@gmail.com>
2023-09-07 07:26:45 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - contains the main function
+ * Return: the value 0
+ */
+int main(void)
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
