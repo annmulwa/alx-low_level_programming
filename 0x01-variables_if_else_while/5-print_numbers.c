4fcde06dca8f8861ba327e8a8378799a2245a179
annmulwa <annmulwa2000@gmail.com>
2023-09-08 13:13:06 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - contains the main function
+ * Return: the value 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		printf("%d", i);
+			i++;
+	}
+	printf("\n");
+	return (0);
+}
