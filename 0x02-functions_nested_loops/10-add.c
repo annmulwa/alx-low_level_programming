0f5585470e8d6be6479962e092fc7f8053de0c03
annmulwa <annmulwa2000@gmail.com>
2023-09-12 11:58:24 -0700

    10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers
+ * @a: check value in a
+ * @b: check value at b
+ * Return: c
+ */
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
