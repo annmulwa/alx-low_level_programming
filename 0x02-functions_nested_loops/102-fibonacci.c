e4e65be6839d5a29cfc2cd9b18535533cbbb4bf3
annmulwa <annmulwa2000@gmail.com>
2023-09-12 14:53:55 -0700

    102-fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 fibonacci numbers
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	unsigned long int num1 = 1;
+	unsigned long int num2 = 2;
+	unsigned long int num3;
+	int i;
+
+	printf("%lu, ", num1);
+
+	for (i = 1; i <= 50; i++)
+	{
+		printf("%lu", num2);
+		num3 = num1 + num2;
+		num1 = num2;
+		num2 = num3;
+		if (i != 50)
+			printf(", ");
+	}
+	printf("\n");
+	return (0);
+}
