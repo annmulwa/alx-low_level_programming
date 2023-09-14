5e9484cf79cb1588978d88894a4dad0602826d2d
annmulwa <annmulwa2000@gmail.com>
2023-09-14 04:13:23 -0700

    7-diagonals

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,8 +12,7 @@ void print_diagonal(int n)
 		_putchar('\n');
 	else
 	{
-		int i;
-		int j;
+		int i, j;
 
 		for (i = 0; i < n; i++)
 		{
@@ -24,5 +23,4 @@ void print_diagonal(int n)
 		}
 
 	}
-	_putchar('\n');
 }
