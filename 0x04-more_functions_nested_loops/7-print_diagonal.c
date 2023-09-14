5d1d7f690583db4ad35febd7a6a5eb5754775c2c
annmulwa <annmulwa2000@gmail.com>
2023-09-14 03:57:52 -0700

    7-diagonals

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints the backslash
+ * @n: check the value
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		int i;
+		int j;
+
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 0; j <= i; j++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+
+	}
+}
