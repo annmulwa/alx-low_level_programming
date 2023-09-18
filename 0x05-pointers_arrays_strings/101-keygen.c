3de17eb2246a553c0538f40d4d8e3b5205a9ef8b
annmulwa <annmulwa2000@gmail.com>
2023-09-18 10:02:58 -0700

    101-keygen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - program that generates random password
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int pass[100];
+	int j, sum, m;
+
+	sum = 0;
+
+	srand(time(NULL));
+
+	for (j = 0; j < 100; j++)
+	{
+		pass[j] = rand() % 78;
+		sum += (pass[j] + '0');
+		putchar(pass[j] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			m = 2772 - sum - '0';
+			sum += m;
+			putchar(m + '0');
+			break;
+		}
+	}
+
+	return (0);
+}
+
