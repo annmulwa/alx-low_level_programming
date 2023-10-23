7d0f6c06695eaf76bd48400920d5f5a0bb2ab633
annmulwa <annmulwa2000@gmail.com>
2023-10-23 06:19:21 -0700

    print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints the integers in a node
+ * @h: head
+ * Return: number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+        size_t nodes = 0;
+
+        while (h != 0)
+        {
+                printf("%d\n", h->n);
+                nodes++;
+                h = h->next;
+        }
+        return (nodes);
+}
