d59f903e3ee12108d96c0df20e5de745ed4fa2be
annmulwa <annmulwa2000@gmail.com>
2023-10-24 03:48:15 -0700

    free first node

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the first node
+ * @head: address of the nodes
+ * Return: the head's node data
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *tmp;
+	int n;
+
+	if (head == NULL)
+		return (0);
+	n = (*head)->n;
+	tmp = *head;
+	*head = (*head)->next;
+	free(tmp);
+
+	return (n);
+
+}
