6bc7bee9ae7c68e70141ba87184add4e4cb56184
annmulwa <annmulwa2000@gmail.com>
2023-10-30 05:07:19 -0700

    read

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads contents of a textfile and prints the
+ * content to the std output
+ * @filename: allocates memory for the string to be printed
+ * @letters: letters to be printed
+ * Return: number of letters read
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *readfile;
+	char *filesize;
+	int lettersnum;
+
+	if (filename == NULL)
+		return (0);
+	readfile = fopen(filename, "r");
+	if (readfile == NULL)
+		return (0);
+	filesize = malloc(sizeof(char) * (letters + 1));
+	if (filesize == NULL)
+	{
+		fclose(readfile);
+		return (0);
+	}
+	lettersnum = fread(filesize, sizeof(char), letters, readfile);
+	if (lettersnum == -1)
+	{
+		free(filesize);
+		fclose(readfile);
+		return (0);
+	}
+	filesize[lettersnum] = '\0';
+	printf("%s", filesize);
+	free(filesize);
+	fclose(readfile);
+	return (lettersnum);
+}
