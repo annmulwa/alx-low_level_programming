6dc055da0f76618524e263dd177b7eafd2de7bdd
annmulwa <annmulwa2000@gmail.com>
2023-12-11 03:21:25 -0800

    CPython

---
+++ 0x18-dynamic_libraries/mod.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * mod - returns modulus of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: modulus of the two integers
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
